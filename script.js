f86c39466e640380dbab7a9d5d4939ba93a26128
Br0cod3 <thespammingbott@gmail.com>
2025-07-14 16:38:14 +0100

    refactored the entire code for the calculator

--- script.js
+++ script.js
@@ -1,34 +1,63 @@
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function divide(a, b) {
-  return b === 0 ? "undefined" : a / b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function operate(a, b, operator) {
-  return operator(a, b);
-}
-
+let lowerDisplay = document.querySelector(".lower");
+let upperDisplay = document.querySelector(".upper");
+let ac = document.querySelector("#ac");
 let buttons = document.querySelectorAll(".btn");
+let operators = document.querySelectorAll(".opr");
+let equal = document.querySelector("#equals");
+
+const buttonActions = {
+  digit: (value) => updateDisplay(value),
+  operation: (opr) => setOperation(opr),
+  equals: () => calculateResult(),
+  clear: () => removeLastDigit(),
+  AC: () => clearDisplay(),
+};
+
+const calculatorState = {
+  currentValue: "",
+  operator: "",
+  previousValue: "",
+};
+
+const operations = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => (b === 0 ? "undefined" : a / b),
+};
+
+function updateDisplay(value) {
+  calculatorState.currentValue += value.textContent;
+  lowerDisplay.textContent = calculatorState.currentValue;
+}
+
+function setOperation(operation) {
+  calculatorState.operator = operation;
+  updateDisplay(calculatorState.operator)
+}
+
+function calculateResult() {}
+
+function removeLastDigit() {}
+
+function clearDisplay() {
+  lowerDisplay.textContent = "";
+  upperDisplay.textContent = "";
+  calculatorState.currentValue = "";
+  calculatorState.previousValue = "";
+  calculatorState.operator = "";
+}
+
 buttons.forEach((element) => {
-  element.addEventListener("click", addToDisplay);
+  element.addEventListener("click", () => buttonActions.digit(element));
 });
 
-let operators = document.querySelectorAll(".opr");
 operators.forEach((element) => {
   if (element.textContent !== "=") {
-    element.addEventListener("click", addToDisplay);
+    element.addEventListener("click", () => buttonActions.operation(element));
   }
 });
 
-let equal = document.querySelector("#equals");
-equal.addEventListener("click", func);
+ac.addEventListener("click", () => buttonActions.AC());
+
+equal.addEventListener("click", () => buttonActions.equals());
